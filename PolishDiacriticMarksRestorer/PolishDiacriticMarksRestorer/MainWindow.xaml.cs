a5194f84e51493e1ff49562af51bc9a3d654fe4a
Radosław Taborski <radoslaw.taborski@gmail.com>
2018-01-31 12:04:22 +0100

    init commit

---
+++ PolishDiacriticMarksRestorer/PolishDiacriticMarksRestorer/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Resources;
+
+namespace PolishDiacriticMarksRestorer
+{
+    /// <inheritdoc cref="MainWindow" />
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton != MouseButton.Left) return;
+            if (e.ClickCount == 2)
+            {
+                AdjustWindowSize();
+            }
+            else
+            {
+                if (Application.Current.MainWindow != null) Application.Current.MainWindow.DragMove();
+            }
+        }
+
+        private void AdjustWindowSize()
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+                Uri resourceUri = new Uri("img/max.png", UriKind.Relative);
+                StreamResourceInfo streamInfo = Application.GetResourceStream(resourceUri);
+
+                if (streamInfo != null)
+                {
+                    BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+                    var brush = new ImageBrush();
+                    brush.ImageSource = temp;
+                    MaxButton.Background = brush;
+                }
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+                Uri resourceUri = new Uri("img/min.png", UriKind.Relative);
+                StreamResourceInfo streamInfo = Application.GetResourceStream(resourceUri);
+
+                if (streamInfo != null)
+                {
+                    BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+                    var brush = new ImageBrush();
+                    brush.ImageSource = temp;
+                    MaxButton.Background = brush;
+                }
+            }
+
+        }
+
+        private void MinButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaxButton_Click(object sender, RoutedEventArgs e)
+        {
+            AdjustWindowSize();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
